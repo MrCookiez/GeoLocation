172ac9efe9e6efe67840aeffc95b89f01b64654b
Theodoros Vragkos <bigt3ddybear@yahoo.gr>
2018-08-11 22:34:50 +0300

    Add files via upload

---
+++ data.php
@@ -0,0 +1,53 @@
+   <?php 
+   
+   //"GetCityDetails" code sample to get visitors IP ADDRESS
+   function getIP() {
+      foreach (array(
+          'HTTP_CLIENT_IP', 
+          'HTTP_X_FORWARDED_FOR', 
+          'HTTP_X_FORWARDED', 
+          'HTTP_X_CLUSTER_CLIENT_IP', 
+          'HTTP_FORWARDED_FOR', 
+          'HTTP_FORWARDED', 
+          'REMOTE_ADDR') as $key) {
+         if (array_key_exists($key, $_SERVER) === true) {
+            foreach (explode(',', $_SERVER[$key]) as $ip) {
+               if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+                  return $ip;
+               }
+            }
+         }
+      }
+   }
+   // Get the result of getIP function and place it into $ip variable
+   $ip = getIP();
+
+   //Use REST API to get visitors data by URL + User's current IP ADDRESS from previous var
+   $json = file_get_contents('http://getcitydetails.geobytes.com/GetCityDetails?fqcn='.$ip); 
+
+   //"json_decode()" will take JSON and convert it into a PHP array, "true" == Assosiated Array
+   $data = json_decode($json, true);
+
+   //Test the result
+   //echo '<b>'. $ip .'</b> resolves to:'. print_r($data);
+
+   //Assosiate Array with selected data from REST API of "geobytes"
+   $details = array(
+        //Basic Information
+        "ip"=>$data["geobytesipaddress"],
+        "fullName"=>$data["geobytesfqcn"],
+        "short"=>$data["geobytesregionlocationcode"],
+        //More Information (will be displayed only in Premium Tab)
+        "country"=>$data["geobytescountry"],
+        "city"=>$data["geobytescity"],
+        "lat"=>$data["geobyteslatitude"],
+        "long"=>$data["geobyteslongitude"],
+        "timezone"=>$data["geobytestimezone"],
+        "currency"=>$data["geobytescurrency"]        
+        );
+    //Output data for external use in JSON format
+    $api = json_encode($details);
+    
+    echo print_r($api, true);
+
+?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<!-- Some CSS-->
+<style>
+    h2 {
+        text-align: center;
+    }
+    .center {
+        text-align: center;
+        margin: 20px auto;
+        background-color: #ccc;
+        padding: 40px;
+    }
+    .content {
+        width: 100%;
+    }
+    .info {
+        margin: auto;
+        max-width: 350px;
+    }
+</style>
+
+</head>
+<body>
+    
+    <!-- API LINK-->
+    <hr>
+    <div class="center">
+        <a class="center" href="data.php">Link to API</a>
+    </div>
+    <hr>
+    <br>
+    <!-- HIDE JSON-->
+    <div style="display: none;">
+        <?php include_once "data.php"; ?>
+    </div>
+    <div class="content">
+
+        <!-- DISPLAY DATA ON PAGE -->
+        <div class="info">
+        
+        <h2>GET VISITORS LOCATION</h2>
+            <h4>IP ADDRESS:             <?php echo $details["ip"]; ?></h4>
+            <h4>Country (Full Name):    <?php echo $details["fullName"]; ?></h4>
+            <h4>Short name (initials) : <?php echo $details["short"]; ?></h4>
+        </div>
+
+    </div>
+    
+
+</body>
+</html>
